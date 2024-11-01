c0ca352f33d92951239ed09da1aafc6b26395d76
kellysabioni <kelly.sabioni@yahoo.com.br>
2024-11-01 06:15:04 -0300

    Versão final PI
    Ajustes faltantes
    - Botão flutuante Whatsapp
    - Configuração rodapé (quebra no responsivo)

--- js/script.js
+++ js/script.js
@@ -23,3 +23,38 @@ const detailsElements = document.querySelectorAll('details');
                 }
             });
         });
+
+const formulario = document.querySelector("form");
+const campoTelefone = formulario.querySelector("#telefone");
+$(campoTelefone).mask("(00) 00000-0000");
+
+var form = document.getElementById("form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Obrigado! Mensagem enviada com sucesso. Aguarde nosso retorno. 🚗";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Parece que houve um problema ao enviar seu formulário. Por favor, tente novamente mais tarde."
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
